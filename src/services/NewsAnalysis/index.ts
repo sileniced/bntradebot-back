ba1debdc8e2289aeb8e4192a35d50fd43f3fa091
Vince Liem <vincepaul.liem@gmail.com>
2019-04-15 21:48:10 +0200

    started NewsAnalysis

--- src/services/NewsAnalysis/index.ts
+++ src/services/NewsAnalysis/index.ts
@@ -29,50 +29,18 @@ export default async pairs => {
       }
   }, []).join(',')
 
-  const fetchResult = await Promise.all([1, 2, 3, 4, 5].map(page => request.get(`https://cryptopanic.com/api/v1/posts/?auth_token=${CryptoPanicAPI}&currencies=${symbols}&filter=hot&page=${page}`))
-  )
-
-  const example: CryptoPanicPost = {
-    'kind': 'news',
-    'domain': 'investinblockchain.com',
-    'votes': {
-      'negative': 0,
-      'positive': 6,
-      'important': 0,
-      'liked': 7,
-      'disliked': 1,
-      'lol': 9,
-      'toxic': 0,
-      'saved': 0
-    },
-    'source': {
-      'title': 'Invest in Blockchain',
-      'region': 'en',
-      'domain': 'investinblockchain.com',
-      'path': null
-    },
-    'title': 'Vitalik: “Craig Wright Absolutely Should Have A Voice. But So Do All Of Us Laughing At His Stupidity.”',
-    'published_at': '2019-04-15T09:25:12Z',
-    'slug': 'Vitalik-Craig-Wright-Absolutely-Should-Have-A-Voice-But-So-Do-All-Of-Us-Laughing-At-His-Stupidity',
-    'currencies': [
-      {
-        'code': 'ETH',
-        'title': 'Ethereum',
-        'slug': 'ethereum',
-        'url': 'https://cryptopanic.com/news/ethereum/'
-      }
-    ],
-    'id': 5131638,
-    'url': 'https://cryptopanic.com/news/5131638/Vitalik-Craig-Wright-Absolutely-Should-Have-A-Voice-But-So-Do-All-Of-Us-Laughing-At-His-Stupidity',
-    'created_at': '2019-04-15T09:25:12Z'
-  }
+  const fetchResult = await Promise.all([1, 2, 3, 4, 5].map(page => request.get(`https://cryptopanic.com/api/v1/posts/?auth_token=${CryptoPanicAPI}&currencies=${symbols}&filter=hot&page=${page}`)))
 
   fetchResult.map(result => result.body.results.map(({votes, currencies, title, created_at}: CryptoPanicPost) => {
-    const age = Date.now() - Date.parse(created_at)
-    const votes = {
-      bull: votes.positive * 1.2 + votes.liked + votes.lol * 0.8,
-      bear: votes.negative * 1.2 + votes.disliked + votes.toxic * 2,
-      important: votes.important
+    const analysis = {
+      age: Date.now() - Date.parse(created_at),
+      votes: {
+        bull: votes.positive * 1.2 + votes.liked + votes.lol * 0.8,
+        bear: votes.negative * 1.2 + votes.disliked + votes.toxic * 2,
+        important: votes.important
+      }
     }
+
+    return analysis
   }))
 }
